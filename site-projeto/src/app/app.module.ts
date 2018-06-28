e8b9ebfc86472ab66793c6dbbd6f1f798cdf7337
Ana Flavia de Araujo <aflvaraujo@gmail.com>
2018-06-27 22:05:22 -0300

    front-end changes
    added copy to main page

--- site-projeto/src/app/app.module.ts
+++ site-projeto/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import  {MatToolbarModule } from '@angular/material/toolbar';
+ 
 
 import { AppComponent } from './app.component';
+import { Browser } from 'protractor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
