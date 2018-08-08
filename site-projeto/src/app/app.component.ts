19b96fb66f83490fcf8abd5adb7f247a3de440ed
Ana Flavia de Araujo <aflvaraujo@gmail.com>
2018-08-07 21:03:17 -0300

    add course checkbox

--- site-projeto/src/app/app.component.ts
+++ site-projeto/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  courses: Courses[] = [
+    { name: 'Engenharias' }
+    , { name: 'Engenharia Aeroespacial' }
+    , { name: 'Engenharia Ambiental' }
+    , { name: 'Engenharia Biomédica' }
+    , { name: 'Engenharia de Energia' }
+    , { name: 'Engenharia de Gestão' }
+    , { name: 'Engenharia de Informação' }
+    , { name: 'Engenharia de Instrumentação, Automação e Robótica' }
+    , { name: 'Engenharia de Materiais' }
+  ]
+
+}
+
+export class Courses {
+  name: string;
 }
